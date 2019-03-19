b79eb065b3bd8a7f9c1df5c9163883e1ddbf4e26
Boyan Kushlev <boyan.kushlev@paysafe.com>
2019-03-19 20:04:52 +0200

    initial commit: add eureka discovery config

---
+++ src/test/java/com/hippo/coresubmission/CoreSubmissionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hippo.coresubmission;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoreSubmissionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
